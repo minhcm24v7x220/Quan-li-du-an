ceb3e0aaef5a2de140a14763eb20f23c8ec58a38
minhcm24v7x220 <minhcm24v7x220@dttx.ctu.edu.vn>
2025-10-31 13:32:46 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cor());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
+app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+ // handle 404 response
+app.use((req, res, next) => {
+ // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+ // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+ // Middleware xử lý lỗi tập trung.
+});
+
+ // define error-handling middleware last, after other app.use() and routes calls
+ // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử 
+return res.status(error.statusCode || 500).json({
+message: error.message || "Internal Server Error",
+});
+module.exports = app;
